8a28abbba2ecbdd048173ed1f0c0ecd540658627
Sayyed Arib Hussain <sayyedaribhussain4321@gmail.com>
2023-03-12 11:43:50 +0530

    project section updated

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -2,12 +2,9 @@ import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import ProjectCard from "./ProjectCards";
 import Particle from "../Particle";
-import leaf from "../../Assets/Projects/leaf.png";
-import emotion from "../../Assets/Projects/emotion.png";
-import editor from "../../Assets/Projects/codeEditor.png";
-import chatify from "../../Assets/Projects/chatify.png";
-import suicide from "../../Assets/Projects/suicide.png";
-import bitsOfCode from "../../Assets/Projects/blog.png";
+import donorLink from "../../Assets/Projects/donorLink.jpg";
+import textUtils from "../../Assets/Projects/textUtils.jpg";
+import simonGame from "../../Assets/Projects/simonGame.jpg";
 
 function Projects() {
   return (
@@ -23,23 +20,34 @@ function Projects() {
         <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={chatify}
+              imgPath={donorLink}
               isBlog={false}
-              title="Chatify"
-              description="Personal Chat Room or Workspace to share resources and hangout with friends build with react.js, Material-UI, and Firebase. Have features which allows user for realtime messaging, image sharing as well as supports reactions on messages."
-              ghLink="https://github.com/soumyajit4419/Chatify"
-              demoLink="https://chatify-49.web.app/"
+              title="DonorLink"
+              description=" (backend has not been deployed yet so demo won't work properly) Unified platform of donation where volunteer can join us and people can donate food and cloth. Instant email will be sent to the nearest volunteer "
+              ghLink="https://github.com/sayyedarib/Donation"
+              demoLink="https://donation-frontend-olive.vercel.app/"
             />
           </Col>
 
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={bitsOfCode}
+              imgPath={textUtils}
               isBlog={false}
-              title="Bits-0f-C0de"
-              description="My personal blog page build with Next.js and Tailwind Css which takes the content from makdown files and renders it using Next.js. Supports dark mode and easy to write blogs using markdown."
-              ghLink="https://github.com/soumyajit4419/Bits-0f-C0de"
-              demoLink="https://blogs.soumya-jit.tech/"
+              title="Text-Utils"
+              description="A static webpage made by using react. Just write or paste the text and count number of words, reading time, convert lower to uppercase, uppercase to lowercase"
+              ghLink="https://github.com/sayyedarib/TextUtils"
+              demoLink="https://text-utils-eight-gilt.vercel.app/"
+            />
+          </Col>
+
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={simonGame}
+              isBlog={false}
+              title="Simon Game"
+              description="A simple game made by using javascript. Try to memorize the pattern"
+              ghLink="https://github.com/sayyedarib/Simon-Game"
+              demoLink="https://sayyedarib.github.io/Simon-Game/"
             />
           </Col>
 
